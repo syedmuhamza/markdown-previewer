168b65db4f2b24d2bc8ea105036d7aa2df4f85a6
Abbas <syedmuhamza@gmail.com>
2023-04-14 04:49:33 +0500

    freeCodeCamp markdown previewer with all passing tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
+import { useState,useEffect } from 'react';
 import './App.css';
+import {marked} from 'marked';
+
+marked.setOptions({
+  breaks:true
+})
+
+const renderer = new marked.Renderer();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Writer />
+    </>
   );
 }
+
+const Writer=()=>{
+
+  const onChangeHandler=(e)=>{
+  setText(e.target.value);
+}
+const initialString=`# This is h1
+
+## This is h2
+
+Here is a link [AAES](https://aaespak.com)
+
+In-line code: \`console.log('hello world');\`
+Some function: 
+\`\`\`
+function add(a, b) {
+return a + b;
+}
+\`\`\`
+
+This is a list
+- item
+- item
+- item
+
+Here is a blockquote:
+
+> This is a quote.
+
+Here is an image:
+
+![alt text](https://via.placeholder.com/150)
+
+Here is some **bolded text**.
+
+
+`
+const [text,setText]=useState(initialString);
+
+useEffect(()=>{
+  console.log(text)
+},[text]);
+
+  return(
+  <div className='main-container'>
+    <div className='container'>
+    <div className='top-tab'>
+      <h5 style={{fontSize:'1rem'}}>Editor</h5>
+    </div>
+    <div className='input-area'>
+      <textarea name="input-field" id="editor" cols="75" rows="10" placeholder='input text here' onChange={onChangeHandler}>
+        {text}
+      </textarea>
+    </div>
+  </div>
+  
+  <div  className='container reader'>
+    <div className='top-tab'>
+      <h5 style={{fontSize:'1rem'}}>Previewer</h5>
+    </div>
+    <Preview id='preview' markdown={text} />
+  </div>
+  </div>
+  );
+};
+
+function Preview({markdown}){
+  return (<div id='preview' dangerouslySetInnerHTML={{
+    __html:marked(markdown,{renderer:renderer})
+  }}>
+
+  </div>)
+}
+
+
 
 export default App;
